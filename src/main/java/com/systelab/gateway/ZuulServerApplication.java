0457938f890258558cdee9935b897ca12d6d1528
alfonsserra <alfonsoserra@gmail.com>
2018-12-01 08:26:39 +0100

    Repackage

--- src/main/java/com/systelab/gateway/ZuulServerApplication.java
+++ src/main/java/com/systelab/gateway/ZuulServerApplication.java
@@ -1,4 +1,4 @@
-package com.systelab.zuul;
+package com.systelab.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/systelab/gateway/ZuulServerApplicationTest.java
+++ src/test/java/com/systelab/gateway/ZuulServerApplicationTest.java
@@ -1,4 +1,4 @@
-package com.systelab.zuul;
+package com.systelab.gateway;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
